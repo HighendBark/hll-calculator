de5db1ff57ae5c048306d3dadea22b8d7961ae88
olfspnnr <olaf@uspinner.de>
2021-10-08 00:26:56 +0200

    hook to get mil

--- src/hooks/useMil.tsx
+++ src/hooks/useMil.tsx
@@ -1,17 +1,30 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Team } from "../types/Team";
 import useUSSRMil from "./useUSSRMil";
-import useStandardMil from "./useUSSRMil";
+import useStandardMil from "./useStandardMil";
 
 const useMil = (team: Team, distance: number | null) => {
+
+  const [selection, setSelection] = useState(0);
+
   const standard = useStandardMil(distance ?? 0);
   const ussr = useUSSRMil(distance ?? 0);
 
-  const selection = useMemo(() => {
-    if(distance === null) return 0;
-    if(team === "standard") return standard;
-    if(team === "ussr") return ussr;
+  useEffect(() => {
+    if(distance === null){
+      setSelection(0);
+    }
+    if(team === "standard") {
+      setSelection(standard);
+    } 
+    if(team === "ussr"){
+      setSelection(ussr);
+    }
   }, [distance, team, standard, ussr])
+
+  useEffect(() => {
+    console.log(standard, ussr);
+  }, [standard, ussr])
 
   return selection;
 }
