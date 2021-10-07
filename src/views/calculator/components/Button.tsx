f2b7d531615d16b6ccc383d65eb37376e899b1c8
olfspnnr <olaf@uspinner.de>
2021-10-08 01:04:32 +0200

    modfied button style

--- src/views/calculator/components/Button.tsx
+++ src/views/calculator/components/Button.tsx
@@ -14,7 +14,7 @@ const Button = (props: ButtonProps) => {
   return (
     <button
       disabled={props.isDisabled}
-      className="w-full inline-flex justify-center items-center py-5 bg-gray-100 rounded-md font-semibold hover:bg-gray-300 disabled:pointer-events-none transition-all ease-in-out duration-150 disabled:brightness-50"
+      className="w-full inline-flex justify-center items-center py-5 bg-gray-100 rounded-md font-semibold hover:bg-gray-300 disabled:pointer-events-none transition-all ease-in-out duration-150 disabled:brightness-50 outline-none focus:outline-none"
       onClick={handleClick}
     >
       <span className="pointer-events-none">
