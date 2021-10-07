e6f2583bf8ede4d759a2e8ffb32f0a1f7c9c3941
olfspnnr <olaf@uspinner.de>
2021-10-08 00:27:55 +0200

    button component; history component;

--- src/views/calculator/components/Button.tsx
+++ src/views/calculator/components/Button.tsx
@@ -3,13 +3,24 @@ import { FC, useCallback } from "react";
 type ButtonProps = {
   value: number;
   onClick: (number: number) => void;
-}
+  isDisabled?: boolean;
+};
 
 const Button = (props: ButtonProps) => {
   const handleClick = useCallback(() => {
     props.onClick(props.value);
-  }, [])
+  }, []);
 
-  return <button className="w-full inline-flex justify-center items-center py-5 bg-gray-100 rounded-md font-semibold hover:bg-gray-200 transition-all ease-in-out duration-150" onClick={handleClick}>{props.value === -1 ? "Reset" : props.value}</button>
-}
+  return (
+    <button
+      disabled={props.isDisabled}
+      className="w-full inline-flex justify-center items-center py-5 bg-gray-100 rounded-md font-semibold hover:bg-gray-300 disabled:pointer-events-none transition-all ease-in-out duration-150 disabled:brightness-50"
+      onClick={handleClick}
+    >
+      <span className="pointer-events-none">
+        {props.value === -1 ? "Reset" : props.value}
+      </span>
+    </button>
+  );
+};
 export default Button;
--- src/views/calculator/components/History.tsx
+++ src/views/calculator/components/History.tsx
@@ -1,18 +1,33 @@
+import { Team } from "../../../types/Team";
+
+export type HistoryEntry = { distance: number; value: number; team: Team };
+
 type HistoryProps = {
-  history: {distance: number, value: number}[]
-}
+  history: HistoryEntry[];
+};
 
 const History = (props: HistoryProps) => {
-
   const hasEntries = props.history.length > 0;
 
-  return <ul className="inline-flex flex-col p-2 mt-2 rounded bg-gray-800">
-    { hasEntries ? props.history.map(({distance,value}, idx) => <li key={idx} className="inline-flex w-full justify-stretch items-center">
-      <span>{distance}</span>
-      <span>{value}</span>
-    </li>) : <span className="text-gray-50 mx-auto text-sm font-semibold tracking-wide">Jetzt loslegen</span>}
-  </ul>
-
-}
+  return (
+    <ul className="inline-flex flex-col p-2 mt-2 rounded bg-gray-800">
+      {hasEntries ? (
+        props.history.map(({ distance, value }, idx) => (
+          <li
+            key={idx}
+            className="inline-flex w-full justify-stretch items-center"
+          >
+            <span>{distance}</span>
+            <span>{value}</span>
+          </li>
+        ))
+      ) : (
+        <span className="text-gray-50 mx-auto text-xs font-semibold tracking-wide">
+          Jetzt loslegen und hier die Werte sehen
+        </span>
+      )}
+    </ul>
+  );
+};
 
 export default History;
