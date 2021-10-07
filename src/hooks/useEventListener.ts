72bbabe8d49a2c3d4c7873e7d10e47ba817e186d
olfspnnr <olaf@uspinner.de>
2021-10-08 01:42:14 +0200

    dont ref eventhandler;

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -1,17 +1,19 @@
 import { useEffect, useRef } from "react";
 
-const useEventListener = <T>(event: string, eventHandler: (ev: T) => void, target: HTMLElement | Window = window) => {
-  const eventHandlerRef = useRef<typeof eventHandler | null>(eventHandler);
-
+const useEventListener = <T>(
+  event: string,
+  eventHandler: (ev: T) => void,
+  target: HTMLElement | Window = window
+) => {
   useEffect(() => {
-    if(target && eventHandlerRef.current){
-      target.addEventListener(event, eventHandlerRef.current as any);
+    if (target) {
+      target.addEventListener(event, eventHandler as any);
       return () => {
-        if(target && eventHandlerRef.current) {
-          target.removeEventListener(event, eventHandlerRef.current as any);
+        if (target) {
+          target.removeEventListener(event, eventHandler as any);
         }
-      }
+      };
     }
-  }, [eventHandler, event])
-}
+  }, [eventHandler, event]);
+};
 export default useEventListener;
