387892c462b3d66f1fe1ead2121b7a4f30166ddc
olfspnnr <olaf@uspinner.de>
2021-10-08 00:27:45 +0200

    design; handling of numbers and edge cases;

--- src/views/calculator/CalculatorView.tsx
+++ src/views/calculator/CalculatorView.tsx
@@ -1,3 +1,4 @@
+import useNumpadKeys from "../../hooks/useNumpadKeys";
 import { TeamLabel, Teams } from "../../types/Team";
 import useCalculatorViewModel from "./CalculatorViewModel";
 import { CalculatorViewTypes } from "./CalculatorViewTypes";
@@ -7,34 +8,62 @@ import History from "./components/History";
 const CalculatorView = (props: CalculatorViewTypes.Props) => {
   const viewModel = useCalculatorViewModel(props);
 
-  const options = Teams.map((team) => ({value: team, label: TeamLabel[team]}));
-  const buttons = Array(9).fill(null).map((_,idx) => idx + 1);
+  useNumpadKeys(viewModel.addToDistance);
 
+  const options = Teams.map((team) => ({
+    value: team,
+    label: TeamLabel[team],
+  }));
 
+  const buttons = Array(9)
+    .fill(null)
+    .map((_, idx) => idx + 1);
 
-  return <article className="inline-flex flex-col mx-auto mt-3 select-none">
-    <div className="inline-flex flex-col p-2 bg-gray-800 rounded">
-    <div className="inline-flex mb-2">
-      <select className="w-full h-full p-1 py-2 bg-gray-100">
-        {options.map(({value, label}) => <option value={value} key={value} label={label} />)}
-      </select>
-    </div>
-      <div className="inline-flex gap-1">
-        <output className="w-32 bg-gray-600 p-2 text-2xl text-gray-50 font-mono tabular-nums">{viewModel.distanceNumbers ?? "0"}</output>
-        <output className="w-32 bg-yellow-500 p-2 text-2xl font-mono tabular-nums">{viewModel.mil}</output>
+  return (
+    <article className="inline-flex flex-col mx-auto mt-3 select-none">
+      <div className="inline-flex flex-col p-2 bg-gray-800 rounded">
+        <div className="inline-flex mb-2">
+          <select
+            onChange={viewModel.changeTeam}
+            className="w-full h-full p-1 py-2 bg-gray-100"
+          >
+            {options.map(({ value, label }) => (
+              <option value={value} key={value} label={label} />
+            ))}
+          </select>
+        </div>
+        <div className="inline-flex gap-1">
+          <output className="inline-flex justify-start items-baseline w-32 bg-gray-600 p-2 text-2xl text-gray-50 font-mono tabular-nums">
+            <span>{viewModel.distanceNumbers ?? "0"}</span>
+            <small className="tracking-tight text-sm ml-auto">m</small>
+          </output>
+          <output className="inline-flex justify-start items-baseline w-32 bg-yellow-500 p-2 text-2xl font-mono tabular-nums">
+            <span>
+              {viewModel.distanceNumbers && +viewModel.distanceNumbers > 100
+                ? viewModel.mil
+                : "0"}
+            </span>
+            <small className="tracking-tight text-sm ml-auto">mil</small>
+          </output>
+        </div>
+        <div className="inline-grid grid-cols-3 mt-2 gap-1">
+          {buttons.map((idx) => (
+            <Button value={idx} onClick={viewModel.addToDistance} key={idx} />
+          ))}
+        </div>
+        <div className="inline-grid grid-cols-2 mt-1 gap-1">
+          <Button
+            isDisabled={viewModel.distanceNumbers === null}
+            value={0}
+            onClick={viewModel.addToDistance}
+          />
+          <Button value={-1} onClick={viewModel.resetDistance} />
+        </div>
+        <ul></ul>
       </div>
-      <div className="inline-grid grid-cols-3 mt-2 gap-1">
-        {buttons.map((idx) => <Button value={idx} onClick={viewModel.addToDistance} key={idx} />)}
-      </div>
-      <div className="inline-grid grid-cols-2 mt-2 gap-1">
-        <Button value={0} onClick={viewModel.addToDistance} />
-        <Button value={-1} onClick={viewModel.addToDistance} />
-      </div>
-      <ul>
-      </ul>
-    </div>
-    <History history={[]} />
-  </article>
-}
+      <History history={[]} />
+    </article>
+  );
+};
 
 export default CalculatorView;
--- src/views/calculator/CalculatorViewModel.ts
+++ src/views/calculator/CalculatorViewModel.ts
@@ -1,33 +1,63 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
 import useMil from "../../hooks/useMil";
 import { Team } from "../../types/Team";
 import { CalculatorViewTypes } from "./CalculatorViewTypes";
+import { HistoryEntry } from "./components/History";
 
 const useCalculatorViewModel = (props: CalculatorViewTypes.Props) => {
+  const [history, setHistory] = useState<HistoryEntry[]>([]);
   const [selectedTeam, setSelectedTeam] = useState<Team>("standard");
   const [distanceNumbers, setDistanceNumbers] = useState<string | null>(null);
   const distance = useMemo(() => {
-    if(distanceNumbers) {
-      return +distanceNumbers;
-    } else return null;
-  }, [distanceNumbers])
+    if (distanceNumbers) {
+      if (distanceNumbers.length >= 3 && +distanceNumbers > 100) {
+        return +distanceNumbers;
+      }
+    }
+    return null;
+  }, [distanceNumbers]);
 
   const mil = useMil(selectedTeam, distance);
 
-  const addToDistance = useCallback((numb: number) =>{
-    setDistanceNumbers(c => numb === -1 ? null : c ? c + numb : numb + "");
-  }, [])
+  const resetDistance = useCallback(() => {
+    setDistanceNumbers(null);
+  }, []);
+
+  const addToDistance = useCallback((numb: number) => {
+    setDistanceNumbers((c) => (c ? c + numb : numb !== 0 ? numb + "" : null));
+  }, []);
+
+  const addToHistory = useCallback((entry: HistoryEntry) => {
+    setHistory((c) => [...c, entry]);
+  }, []);
+
+  const changeTeam = useCallback((ev: ChangeEvent<HTMLSelectElement>) => {
+    if (ev?.target?.value) {
+      setDistanceNumbers(() => {
+        setSelectedTeam(ev.target.value as Team);
+        return null;
+      });
+    }
+  }, []);
 
   useEffect(() => {
-    if(distanceNumbers !== null ) {
-      if(+distanceNumbers > 1600) {
+    if (distanceNumbers !== null) {
+      if (+distanceNumbers > 1600 || distanceNumbers === "00") {
         setDistanceNumbers(null);
       }
     }
-    
-  }, [distanceNumbers])
+  }, [distanceNumbers]);
 
-  return {distanceNumbers, mil, addToDistance}
-}
+  useEffect(() => {}, []);
+
+  return {
+    distanceNumbers,
+    mil,
+    addToDistance,
+    changeTeam,
+    resetDistance,
+    addToHistory,
+  };
+};
 
 export default useCalculatorViewModel;
