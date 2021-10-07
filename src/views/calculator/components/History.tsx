1d04ef7a079244db40fb4a36b5a0cb54b1bde89a
olfspnnr <olaf@uspinner.de>
2021-10-08 01:04:43 +0200

    modfied history design

--- src/views/calculator/components/History.tsx
+++ src/views/calculator/components/History.tsx
@@ -1,4 +1,4 @@
-import { Team } from "../../../types/Team";
+import { Team, TeamLabel } from "../../../types/Team";
 
 export type HistoryEntry = { distance: number; value: number; team: Team };
 
@@ -10,15 +10,24 @@ const History = (props: HistoryProps) => {
   const hasEntries = props.history.length > 0;
 
   return (
-    <ul className="inline-flex flex-col p-2 mt-2 rounded bg-gray-800">
+    <ul className="inline-flex flex-col p-2 mt-2 gap-px rounded bg-gray-800 w-full">
       {hasEntries ? (
-        props.history.map(({ distance, value }, idx) => (
+        props.history.slice(0, 25).map(({ distance, value, team }, idx) => (
           <li
             key={idx}
-            className="inline-flex w-full justify-stretch items-center"
+            className="inline-grid grid-cols-3 w-full justify-stretch items-center bg-gray-100 text-xs rounded"
           >
-            <span>{distance}</span>
-            <span>{value}</span>
+            <span className="inline-flex justify-center items-center">
+              {TeamLabel[team]}
+            </span>
+            <span className="inline-flex justify-between px-5 items-baseline bg-gray-500 text-gray-50 tabular-nums">
+              <span className="tabular-nums">{distance}</span>
+              <small className="text-gray-100 tracking-wide">m</small>
+            </span>
+            <span className="inline-flex justify-between px-5 items-baseline bg-yellow-500 -mr-px rounded-r">
+              <span className="font-semibold tabular-nums">{value}</span>
+              <small className="text-gray-800 tracking-wide">mil</small>
+            </span>
           </li>
         ))
       ) : (
