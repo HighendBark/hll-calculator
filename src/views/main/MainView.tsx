d7286e5eb9b9abe53d22b9d60362e50c79c64b85
olfspnnr <olaf@uspinner.de>
2021-10-08 01:04:59 +0200

    added text gray 800 as default

--- src/views/main/MainView.tsx
+++ src/views/main/MainView.tsx
@@ -1,21 +1,23 @@
-import { Route, Switch } from "react-router"
+import { Route, Switch } from "react-router";
 import CalculatorView from "../calculator/CalculatorView";
 import SettingsView from "../settings/SettingsView";
-import useMainViewModel from "./MainViewModel"
+import useMainViewModel from "./MainViewModel";
 
 const MainView = () => {
   const viewModel = useMainViewModel();
 
-  return <main className="w-full h-screen inline-flex flex-col antialiased grayscale-0">
-    <Switch>
-      <Route path="/">
-        <CalculatorView {...viewModel} />
-      </Route>
-      <Route path="/settings">
-        <SettingsView {...viewModel} />
-      </Route>
-    </Switch>
-  </main>
-}
+  return (
+    <main className="w-full h-screen inline-flex flex-col antialiased grayscale-0 text-gray-800">
+      <Switch>
+        <Route path="/">
+          <CalculatorView {...viewModel} />
+        </Route>
+        <Route path="/settings">
+          <SettingsView {...viewModel} />
+        </Route>
+      </Switch>
+    </main>
+  );
+};
 
-export default MainView
+export default MainView;
