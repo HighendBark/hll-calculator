ac1f60581ef8223a8cd47eb62f8024eb0219f702
olfspnnr <olaf@uspinner.de>
2021-10-08 02:51:27 +0200

    added marker for mobile/desktop

--- src/views/main/MainView.tsx
+++ src/views/main/MainView.tsx
@@ -3,14 +3,17 @@ import CalculatorView from "../calculator/CalculatorView";
 import SettingsView from "../settings/SettingsView";
 import useMainViewModel from "./MainViewModel";
 import { version } from "../../../package.json";
+import useIsMobile from "../../hooks/useIsMobile";
 
 const MainView = () => {
   const viewModel = useMainViewModel();
+  const isMobile = useIsMobile();
 
   return (
     <main className="w-full h-screen min-h-0 inline-flex flex-col antialiased grayscale-0 text-gray-800 overscroll-contain">
       <span className="fixed bottom-3 left-3 text-xs text-gray-400 dark:text-gray-700">
         {version}
+        {isMobile ? "m" : "d"}
       </span>
       <Switch>
         <Route path="/">
