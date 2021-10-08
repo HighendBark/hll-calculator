af49abdb9d2dcbb4c9425c1d3fb744ce27aa0319
olfspnnr <olaf@uspinner.de>
2021-10-08 02:30:47 +0200

    added version number display

--- src/views/main/MainView.tsx
+++ src/views/main/MainView.tsx
@@ -2,12 +2,16 @@ import { Route, Switch } from "react-router";
 import CalculatorView from "../calculator/CalculatorView";
 import SettingsView from "../settings/SettingsView";
 import useMainViewModel from "./MainViewModel";
+import { version } from "../../../package.json";
 
 const MainView = () => {
   const viewModel = useMainViewModel();
 
   return (
     <main className="w-full h-screen min-h-0 inline-flex flex-col antialiased grayscale-0 text-gray-800 overscroll-contain">
+      <span className="fixed bottom-3 left-3 text-xs text-gray-400 dark:text-gray-700">
+        {version}
+      </span>
       <Switch>
         <Route path="/">
           <CalculatorView {...viewModel} />
